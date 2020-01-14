666800f00afe015c6cc24f2ef02f639951ec7b6d
rabin999 <rabin.bhandari999@gmail.com>
2020-01-15 00:55:58 +0545

    _

--- app/config/envrionments/development/app.ts
+++ app/config/envrionments/development/app.ts
@@ -3,6 +3,7 @@ import AppConfigInterface from '../../interface/app'
 export default <AppConfigInterface> {
     host: "localhost",
     port: 8080,
+    protocol: 'http2',
     debug: false,
     maintenance: false,
     enableCluster: true
